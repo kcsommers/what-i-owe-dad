a68693f99a7fd6c5c1240b84fc12de0a8e7af458
Kacy Sommers <kacysommers@gmail.com>
2022-11-14 14:01:52 -0500

    merge feature/routing

--- src/remoteTypes.d.ts
+++ src/remoteTypes.d.ts
@@ -1,21 +1,27 @@
 ///<reference types="react" />
 
-declare module 'kc_components/ui/Button' {
+declare module 'kc_components/*';
+
+declare module 'kc_components/react/ui/Button' {
   export const Button: React.ComponentType<any>;
 }
 
-declare module 'kc_components/ui/Navbar' {
+declare module 'kc_components/react/ui/Navbar' {
   export const Navbar: React.ComponentType<any>;
 }
 
-declare module 'kc_components/ui/Layout' {
+declare module 'kc_components/react/ui/Layout' {
   export const Layout: React.ComponentType<any>;
 }
 
-declare module 'kc_components/theme' {
+declare module 'kc_components/react/theme' {
   export const BaseTheme: React.ComponentType<any>;
 }
 
-declare module 'kc_components/global-styles' {
+declare module 'kc_components/react/ui/Form' {
+  export const Form: React.ComponentType<any>;
+}
+
+declare module 'kc_components/common/global-styles' {
   export default '';
 }
