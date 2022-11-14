cbdd01683b5e3859bd68ea66f96447f744db2ecc
Kacy Sommers <kacysommers@gmail.com>
2022-11-14 11:41:38 -0500

    initial commit

---
+++ modulefederation.config.js
@@ -0,0 +1,20 @@
+const { dependencies } = require('./package.json');
+
+module.exports = {
+  name: 'host',
+  remotes: {
+    kc_components:
+      'kc_components@http://localhost:4000/kc-components-remoteEntry.js'
+  },
+  shared: {
+    ...dependencies,
+    react: {
+      singleton: true,
+      requiredVersion: dependencies['react']
+    },
+    'react-dom': {
+      singleton: true,
+      requiredVersion: dependencies['react-dom']
+    }
+  }
+};
---
+++ src/remoteTypes.d.ts
@@ -0,0 +1,21 @@
+///<reference types="react" />
+
+declare module 'kc_components/ui/Button' {
+  export const Button: React.ComponentType<any>;
+}
+
+declare module 'kc_components/ui/Navbar' {
+  export const Navbar: React.ComponentType<any>;
+}
+
+declare module 'kc_components/ui/Layout' {
+  export const Layout: React.ComponentType<any>;
+}
+
+declare module 'kc_components/theme' {
+  export const BaseTheme: React.ComponentType<any>;
+}
+
+declare module 'kc_components/global-styles' {
+  export default '';
+}
