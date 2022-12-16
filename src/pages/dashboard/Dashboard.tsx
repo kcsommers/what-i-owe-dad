e1512c04cd44a0ab7bd3bd13949f7fd880ef3154
Kacy Sommers <kacysommers@gmail.com>
2022-12-15 23:01:58 -0500

    add dashboard ppage

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,23 +1,36 @@
-import { ImageCrossfader } from 'kc_components/react/ui/ImageCrossfader';
+import { animated } from '@react-spring/web';
 import { getDollarString } from 'kc_components/common/utils/display/get-dollar-string';
+import { ImageCrossfader } from 'kc_components/react/ui/ImageCrossfader';
 import { Layout } from 'kc_components/react/ui/Layout';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useFirebase } from '../../firebase';
 import { getLoansCollection } from '../../transactions/loans/loans-api';
+import { getPaymentsCollection } from '../../transactions/payments/payments-api';
 
 export const DashboardPage = () => {
   const { firestore } = useFirebase();
-  const [totalLoanAmount, setTotalLoanAmount] = useState(0);
+  const [totalOwed, setTotalOwed] = useState(0);
 
   useEffect(() => {
     (async () => {
-      const loansCollection = await getLoansCollection(firestore);
-      const total = loansCollection.docs.reduce((tot, doc) => {
-        const data = doc.data();
-        tot += data.amount;
-        return tot;
-      }, 0);
-      setTotalLoanAmount(total);
+      const [loansCollection, paymentsCollection] = await Promise.all([
+        getLoansCollection(firestore),
+        getPaymentsCollection(firestore)
+      ]);
+      const totalLoans =
+        loansCollection.docs.reduce((tot, doc) => {
+          const data = doc.data();
+          tot += data.amount;
+          return tot;
+        }, 0) || 0;
+      const totalPayments =
+        paymentsCollection.docs.reduce((tot, doc) => {
+          const data = doc.data();
+          tot += data.amount;
+          return tot;
+        }, 0) || 0;
+
+      setTotalOwed(totalLoans - totalPayments);
     })();
   }, []);
 
@@ -25,14 +38,14 @@ export const DashboardPage = () => {
     <Layout>
       <div className='pos-relative h-100vh'>
         <div className='pos-absolute pos-fill'>
-          <div
+          <animated.div
             className='pos-absolute pos-fill z-2'
             style={{
               backgroundImage:
                 'radial-gradient(circle at center, rgba(0, 0, 0, 0.5) 0, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.05) 100%)',
               backgroundSize: 'cover'
             }}
-          ></div>
+          ></animated.div>
           <ImageCrossfader
             images={[
               {
@@ -46,7 +59,7 @@ export const DashboardPage = () => {
             className='pos-relative z-2 color-white'
             style={{ fontSize: '100px' }}
           >
-            {getDollarString(totalLoanAmount)}
+            {getDollarString(totalOwed)}
           </h1>
         </div>
       </div>
